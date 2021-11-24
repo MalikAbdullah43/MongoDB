6a460bd597f0fa0ea5f0d21ff0705ec4967659fd
MalikAbdullah43 <malikabdullah4300@gmail.com>
2021-11-24 10:22:30 +0500

    Login Request improve

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -28,11 +28,6 @@ class LoginRequest extends FormRequest
             'password'=> [
                 'required',
                 'string',
-                'min:8',              // must be at least 10 characters in length
-                'regex:/[a-z]/',      // must contain at least one lowercase letter
-                'regex:/[A-Z]/',      // must contain at least one uppercase letter
-                'regex:/[0-9]/',      // must contain at least one digit
-                'regex:/[@$!%*#?&]/', // must contain a special character
             ],
         ];
     }
